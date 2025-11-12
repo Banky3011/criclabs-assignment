4a18ed468f244d21c2fb050dad893130c942d33e
Banky3011 <bankbiby@gmail.com>
2025-11-12 19:17:28 +0700

    feat: add Footer

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -1,14 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from "./pages/Login";
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-            </Routes>
+            <div className="relative min-h-screen">
+                <div className="">
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/home" element={<Home />} />
+                    </Routes>
+                </div>
+                <div className="fixed bottom-0 left-0 right-0">
+                    <Footer />
+                </div>
+            </div>
         </BrowserRouter>
     )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import type React from "react";
+
+const Footer: React.FC = () => {
+    return (
+        <>
+            <div className="mt-6 text-center mb-4">
+                <p className="text-xs text-gray-400 ">© Criclabs</p>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
